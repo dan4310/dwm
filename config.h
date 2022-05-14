cd35324d7be488d79146aa25458a7847153a1320
dan4310 <dan4310@gmail.com>
2022-05-14 15:54:55 -0400

    fix(gaps): set gaps with Xresources

--- config.h
+++ config.h
@@ -3,10 +3,10 @@
 /* appearance */
 static unsigned int borderpx = 1; /* border pixel of windows */
 static unsigned int snap     = 32; /* snap pixel */
-static const unsigned int gappih = 20; /* horiz inner gap between windows */
-static const unsigned int gappiv = 10; /* vert inner gap between windows */
-static const unsigned int gappoh = 10; /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov = 30; /* ver outer gap between windwos and screen edge */
+static unsigned int gappih = 20; /* horiz inner gap between windows */
+static unsigned int gappiv = 10; /* vert inner gap between windows */
+static unsigned int gappoh = 10; /* horiz outer gap between windows and screen edge */
+static unsigned int gappov = 30; /* ver outer gap between windwos and screen edge */
 static int smartgaps = 0; /* 1 means no outer gap when there is only one window */
 static int showbar	     = 1; /* 0 means no bar */
 static int topbar	     = 1; /* 0 means bottom bar */
@@ -95,7 +95,11 @@ ResourcePref resources[] = {
 		{ "selbordercolor",     STRING,  &selbordercolor },
 		{ "selfgcolor",         STRING,  &selfgcolor },
 		{ "borderpx",          	INTEGER, &borderpx },
-		{ "snap",          		INTEGER, &snap },
+		{ "gappih",		INTEGER, &gappih   },
+		{ "gappiv",		INTEGER, &gappiv   },
+		{ "gappoh",		INTEGER, &gappoh   },
+		{ "gappov",		INTEGER, &gappov   },
+		{ "snap",          	INTEGER, &snap },
 		{ "showbar",          	INTEGER, &showbar },
 		{ "topbar",          	INTEGER, &topbar },
 		{ "nmaster",          	INTEGER, &nmaster },
